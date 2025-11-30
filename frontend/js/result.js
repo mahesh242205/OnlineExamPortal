971496f1dea60505ec1acf64f1ef4eeeec287bcc
mahesh242205 <mahesh242205@gmail.com>
2025-11-30 21:15:09 +0530

    Add files via upload

---
+++ frontend/js/result.js
@@ -0,0 +1,11 @@
+const urlParams = new URLSearchParams(window.location.search);
+
+const studentName = urlParams.get("studentName");
+const score = urlParams.get("score");
+const total = Number(urlParams.get("total"));
+const percentage = (score/total)*100;
+
+
+document.getElementById("studentName").textContent = `Student : ${studentName}`;
+document.getElementById("score").textContent = `Correct Answer : ${score}`;
+document.getElementById("percentage").textContent = `Percentage : ${percentage}%`;
